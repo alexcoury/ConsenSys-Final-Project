c02edebf971f9406b3ad4b2053a09d3f19ea306f
Alex Coury <alexcoury@MacBook-Pro.localdomain>
2019-07-30 21:48:43 -0400

    Updated comments

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -3,13 +3,17 @@ pragma solidity ^0.5.0;
 //Importing OpenZeppelin's SafeMath Implementation
 import "./SafeMath.sol";
 
+// @title A CrowdFunding contract to create a single campaign
+/// @author Alex Coury
+/// @notice The owner can use this contract to create a campaign and supporters can contribute to it
+/// @dev The CrowdFundingFactory enables fluid intialization of a new crowdfunding campaign
 contract CrowdFunding {
     using SafeMath for uint256;
 
-    address payable public owner;
-    string public title;
+    address payable public owner; //The owner/creator of the campaign
+    string public title; //The title of the project
     mapping(address => bool) contributors; //bool to check if contributor supported a campaign
-    string public description;
+    string public description; //The description of the project
     uint256 public minimumContribution; //minimum constribution a contributor can send set by the creator
     uint256 public targetGoal; //The target goal of the campaign set by the creator
     uint256 public contributorsCount; //count of contributors for a single campaign
@@ -17,9 +21,11 @@ contract CrowdFunding {
 
     bool public stopped = false; //bool for Circuit Breaker
 
-
+    //event to log when a supporter contributes to the campaign
     event LogContribution(address contributor, uint256 value);
-    event LogWithdrawalCompletion(address recipient, uint256 value);
+
+    //logs the withdraw triggered only by the owner
+    event LogWithdrawalCompletion(address owner, uint256 value);
 
     constructor(string memory _title, string memory _description, uint256 _minimum, uint256 _goal, address payable _owner) public {
         title = _title;
@@ -52,11 +58,17 @@ contract CrowdFunding {
          _;
     }
 
-    //Creator can freeze contract if they are aware of a bug
+    /// @author Alex Coury
+    /// @notice Circuit Breaker is a design patterns that allow contract functionality to be stopped if a bug is found
+    /// @dev The stopped bool starts out as false and disables both the contribute and withdraw function when enabled
     function circuitBreaker() public isCreator {
        stopped = !stopped;
     }
 
+    /// @author Alex Coury
+    /// @notice Displays the details of the crowdfunding campaign
+    /// @return The campaign returns a title, description, minmum contributions allowed (set by owner), targeted fundraising goal,
+    //bool for inactive/active, and count of how many supporters
     function readCampaign() public view returns(string memory projectTitle, string memory desc, uint256 min, uint256 projectGoal, bool open, uint256 count){
         projectTitle = title;
         desc = description;
@@ -66,6 +78,9 @@ contract CrowdFunding {
         count = contributorsCount;
     }
 
+   /// @author Alex Coury
+    /// @notice Allows the supporters to contribute to the crowdfunding campaign
+    /// @dev Only the contributor/supporter can contribute
     function contribute() public payable  stopInEmergency isContributor {
         require(isOpen == true, "The crowdfund is no longer open.");
         require(msg.value > minimumContribution, "The contribution must be above the minimum Contribution");
@@ -74,6 +89,10 @@ contract CrowdFunding {
         contributorsCount++;
         emit LogContribution(msg.sender, msg.value);
     }
+
+    /// @author Alex Coury
+    /// @notice Owner can withdraw the funds of the crowdfund raising contract
+    /// @dev Only the creator/owner can withdraw the funds
     function withdraw() public onlyInEmergency isCreator {
         isOpen = false;
         uint transferBalance = address(this).balance;
--- contracts/CrowdFundingFactory.sol
+++ contracts/CrowdFundingFactory.sol
@@ -4,21 +4,26 @@ import "./CrowdFunding.sol";
 import "./SafeMath.sol";
 
 contract CrowdFundingFactory {
-    using SafeMath for uint256;
+    using SafeMath for uint256; //OpenZeppelin's Safe Math Library
 
-    address payable public owner = msg.sender;
-    CrowdFunding[] public deployedCampaigns;
+    address payable public owner = msg.sender; //The creator/owner of the campaign
+    CrowdFunding[] public deployedCampaigns; //list of campaigns created
 
+    //event which logs the creation of a campaign, campaign address is used for testing
     event LogCampaignCreated(uint256 minimum, string title, string desc, uint256 goal, address campaignAddress);
 
-    //Function that allows creator to create a new crowdFunding campaign
+    /// @author Alex Coury
+    /// @notice Intializes the CrowdFunding campaign contract
+    /// @param The details of the project are passed for intialization in the CrowdFunding contract
     function createCampaign(string memory _title, string memory _description, uint256 _minimum, uint256 _goal) public {
         CrowdFunding newCampaign = new CrowdFunding(_title, _description, _minimum, _goal, owner);
         deployedCampaigns.push(newCampaign);
         emit LogCampaignCreated(_minimum, _title, _description, _goal, address(newCampaign));
     }
 
-    //returns a list of campaigns
+    /// @author Alex Coury
+    /// @notice Gets the list of Crowdfunding campaigns that have been created
+    /// @return The function returns an array containing all the deployed addresses
     function getDeployedCampaigns() public view returns (CrowdFunding[] memory) {
         return deployedCampaigns;
     }
