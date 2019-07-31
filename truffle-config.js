5bd78322aaccc103d0829413fbdc1ba8068a2f59
Alex Coury <alexcoury@MacBook-Pro.localdomain>
2019-07-31 19:04:46 -0400

    Succesfully deployed to Rinkeby network

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,9 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "shield goat amateur affair thing apart conduct roast miracle chuckle trim heart";
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
+
   networks: {
     development: {
       host: "127.0.0.1",
@@ -9,6 +12,12 @@ module.exports = {
     },
     develop: {
       port: 8545
-    }
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/844adf6da58c4ebfa65fa85e2de4fe12")
+      },
+      network_id: 4
+    }   
   }
 };
